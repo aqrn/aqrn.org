1d0bce2e09651474ff76e6d51f395f333c9030c4
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-11-03 12:08:16 -0500

    Add form and City class

--- aqrn_project/aqrn_app/services.py
+++ aqrn_project/aqrn_app/services.py
@@ -18,7 +18,38 @@ def get_current_aq(zip_code):
 
     json_object = json.loads(r.text)
 
-    json_formatted_str = json.dumps(json_object, indent=2)
+    return json_object
 
-    return json_formatted_str
 
+def get_max_aqi(report):
+    max_aqi = -1
+    max_aqi_index = -1
+    for i in range(len(report)):
+        pollutant_aqi = report[i]["AQI"]
+        if pollutant_aqi > max_aqi:
+            max_aqi = pollutant_aqi
+            max_aqi_index = i
+    return max_aqi, max_aqi_index
+
+
+class City:
+    def __init__(self, zip_code):
+        self.current_json = get_current_aq(zip_code)
+
+        if len(self.current_json) < 1:
+            return None
+
+        self.reporting_area = self.current_json[0]["ReportingArea"]
+
+        self.aqi, self.aqi_index = get_max_aqi(self.current_json)
+
+        # Get category name and number if they exist, else they will be set to None
+        self.cat_name = self.current_json[self.aqi_index]["Category"].get("Name")
+        self.cat_num = self.current_json[self.aqi_index]["Category"].get("Number")
+
+        # TODO: loop through self.current_json and generate self.full_report
+        # which is displayed below the form
+        # e.g. self.full_report = [[o2, o2 aqi, o2 cat], [pm2.5, pm2.5 aqi, pm2.5 cat]]
+
+        # TODO: get historical data
+        # and save as list of dates and AQI ratings
--- aqrn_project/aqrn_app/views.py
+++ aqrn_project/aqrn_app/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .services import get_current_aq
+from .services import City
+from .forms import ZipCodeForm
+from django.http import Http404
 
 
 def home(request):
-    report = get_current_aq(98121)
-    return HttpResponse(report, content_type='application_json')
+    # report = get_current_aq(98121)
+    # return HttpResponse(report, content_type='application_json')
+
+    # Process form data if POST request
+    if request.method == 'POST':
+
+        form = ZipCodeForm(request.POST)
+
+        if form.is_valid():
+            zip_code = form.cleaned_data['zip_code']
+
+            try:
+                city = City(zip_code)
+            except Exception as e:
+                raise Http404(e)
+
+            if city is not None:
+                body_classes = 'cat'  # TODO: append appropriate cat number
+                return render(request, 'index.html', {
+                    'form': form,
+                    'zip_code': zip_code,
+                    'city': city,
+                    'body_classes': body_classes,
+                })
+
+    else:
+        form = ZipCodeForm()
+
+    return render(request, 'index.html', {'form': form})
