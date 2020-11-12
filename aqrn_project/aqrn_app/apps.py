30d4e1afb889b3d977b9d7c5976d96edea436a53
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-11-11 20:06:18 -0500

    Remove dev project

--- aqrn_project/aqrn_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AqrnAppConfig(AppConfig):
-    name = 'aqrn_app'
--- aqrn_project/local_settings_template.py
+++
@@ -1,4 +0,0 @@
-# rename to local_settings.py
-DEBUG = 'TRUE'
-SECRET_KEY = 'DJANGO SECRET KEY HERE'
-AIR_NOW_API_KEY = 'API KEY HERE'
