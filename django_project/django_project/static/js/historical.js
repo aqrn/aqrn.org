d65a9a0e16386acbc98eaa40367562fa4280285e
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-11-13 11:39:49 -0500

    Add state/time to report, tooltip to chart

--- django_project/django_project/static/js/historical.js
+++ django_project/django_project/static/js/historical.js
@@ -30,15 +30,38 @@
         .domain(d3.extent(aqiArray))
         .range([height, 0]);
 
+    // Create Axis
     let yAxis = d3.axisLeft(yAxisScale).tickFormat( x => {if (Math.floor(x) == x) { return x; }});
     let xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d")).ticks(datesArray.length);
 
+    // Add SVG element to DOM
     let svg = d3.select("#historical .chart").append("svg")
         .attr("preserveAspectRatio", "xMinYMin meet")
         .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
-        .classed("svg-content", true)
-        ;
+        .classed("svg-content", true);
 
+    // Add axis to chart
+    svg.append("g").attr("class", "axis y")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
+        .call(yAxis);
+    svg.append("g").attr("class", "axis x")
+        .attr("transform", `translate(${margin.left}, ${height + margin.top})`)
+        .call(xAxis);
+
+    // Create path generator
+    let line = d3.line()
+        .x(d => x(d.date))
+        .y(d => yAxisScale(d.aqi));
+
+    // Add path to chart
+    let path = svg.append("g").attr("class", "chart-line")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
+        .append("path").attr("d", line(data))
+        .attr("fill", "none")
+        .attr("stroke", "rgba(255,255,255,.4)")
+        .attr("stroke-width", 2);
+
+    // Add circle nodes to chart
     let circles = svg.append("g").attr("class", "nodes")
         .attr("width", width)
         .attr("height", height)
@@ -48,34 +71,43 @@
         .enter().append("circle")
             .attr("cx", (d,i) => x(d.date))
             .attr("cy", (d,i) => height - y(d.aqi))
-            .attr("r", "3")
+            .attr("r", "4")
             .attr("fill", "#efefef");
 
-
-    svg.append("g").attr("class", "axis y")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`)
-        .call(yAxis);
-    svg.append("g").attr("class", "axis x")
-        .attr("transform", `translate(${margin.left}, ${height + margin.top})`)
-        .call(xAxis);
-
-    let line = d3.line()
-        .x(d => x(d.date))
-        .y(d => yAxisScale(d.aqi));
-
-    let path = svg.append("g").attr("class", "chart-line")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`)
-        .append("path").attr("d", line(data))
-        .attr("fill", "none")
-        .attr("stroke", "rgba(255,255,255,.4)");
-
+    // Animate path
     let path_length = path.node().getTotalLength();
-
     path.attr("stroke-dasharray", `${path_length} ${path_length}`)
         .attr("stroke-dashoffset", path_length)
         .transition()
             .duration(2000)
             .ease(d3.easeCubicOut)
             .attr("stroke-dashoffset", 0);
+
+    // Add tooltip element to DOM
+    let tooltip = d3.select('body')
+        .append("div")
+        .attr("class", "tooltip");
+
+    // Tooltip event handlers
+    circles.on('mouseover', function(event, d) {
+        d3.select(".tooltip")
+            .classed("show", true)
+            .html(`${d.date.getMonth()}/${d.date.getDate()}: <span>${d.aqi}</span>`)
+            .style("top", `${event.pageY - 50}px`)
+            .style("left", `${event.pageX - 50}px`)
+
+        d3.select(this).classed("hover", true);
+
+    })
+    .on('mouseout', function() {
+        tooltip.classed("show", false)
+        d3.select(this).classed("hover", false);
+    })
+    .on('mousemove', function(event, d) {
+        d3.select(".tooltip")
+            .style("top", `${event.pageY - 50}px`)
+            .style("left", `${event.pageX - 50}px`)
+    });
+
 
 })();
